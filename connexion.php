76616e96d42f27455118be5dd207fb0b6a275022
Bouchakour <amine.bouchakour@laplateforme.io>
2020-02-02 00:11:50 +0100

    création formulaire

---
+++ connexion.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+<title>Connexion</title>
+
+</head>
+
+
+<form action="" method="post"><br>
+Login : <input type="text" name="login"><br>
+Password : <input type="password" name="password"><br>
+<input type="submit" name="valider">
+</form>
+
+
+<?php
+
+include('fonctions.php');
+connexion();
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+</html>
