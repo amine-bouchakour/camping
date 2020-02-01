19f3b57c5d6d9ddac2d7c9874cc45f78f557e8f5
Bouchakour <amine.bouchakour@laplateforme.io>
2020-02-02 00:12:08 +0100

    création formulaire

---
+++ inscription.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+<title>Inscription</title>
+
+</head>
+
+
+<form action="" method="post"><br>
+Login : <input type="text" name="login"><br>
+Mdp : <input type="password" name="password"><br>
+Conf mdp : <input type="password" name="confirmpassword"><br>
+<input type="submit" name="valider">
+</form>
+
+
+<?php
+
+include('fonctions.php');
+inscription();
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+</html>
