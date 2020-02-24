25d2e642e9bce256e346e4677cc321701dc930f3
Bouchakour <amine.bouchakour@laplateforme.io>
2020-02-24 12:07:57 +0100

    création hyperlien pour circuler sur le site

--- index.php
+++ index.php
@@ -8,7 +8,21 @@
 
 
 
-
+<?php
+session_start();
+if(isset($_SESSION['login'])){
+    echo '<a href="reservations.php">Réservations</a><br>
+    <a href="deconnexion">Déconnexion</a><br>';
+    if($_SESSION['login']=='admin'){
+        echo '<a href="admin.php">Page admin</a><br>';
+    }
+    echo $_SESSION['login'];
+}
+else{
+echo '<a href="connexion.php">Connexion</a><br>
+    <a href="inscription.php">Inscription</a><br>';
+}
+?>
 
 
 
