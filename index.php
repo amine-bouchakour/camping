48e7f8758b8d0c70124226956f50ad349d95a77f
Bouchakour <amine.bouchakour@laplateforme.io>
2020-02-25 16:11:16 +0100

    création tableau recupérant variable facilitant le remplissage du formulaire pour l'utilisateur

--- index.php
+++ index.php
@@ -10,7 +10,31 @@
 	 		<div>
 	 			<?php include('header.php'); ?>
 	 		</div>
+<?php
 
+echo '<table class="tableindex">
+<tr >
+<td></td>
+<td >La Plage</td>
+<td>Les Pins</td>
+<td>Le Maquis</td>
+</tr>
+<tr>
+<td>Camping-car</td>
+<td class="tableindex"><a href="reservations.php?emplacement=plage&amp;habitat=cpgcar">Réserver</a></td>
+<td class="tableindex"><a href="reservations.php?emplacement=pins&amp;habitat=cpgcar">Réserver</a></td>
+<td class="tableindex"><a href="reservations.php?emplacement=maquis&amp;habitat=cpgcar">Réserver</a></td>
+
+</tr>
+<tr>
+<td>Tente</td>
+<td class="tableindex"><a href="reservations.php?emplacement=plage&amp;habitat=tente">Réserver</a></td>
+<td class="tableindex"><a href="reservations.php?emplacement=pins&amp;habitat=tente">Réserver</a></td>
+<td class="tableindex"><a href="reservations.php?emplacement=maquis&amp;habitat=tente">Réserver</a></td>
+</tr>
+</table';
+
+?>
 
 
 			
